6a90c487185e6dcf0caa203c1bd3cd585007bab3
Samiya Akhtar <samiyaakhtar7@gmail.com>
2019-07-13 17:48:27 -0700

    Handle pipelines started in between

--- pipeline-scripts/update_pipeline.py
+++ pipeline-scripts/update_pipeline.py
@@ -1,21 +1,32 @@
 from azure.cosmosdb.table.tableservice import TableService
 from azure.cosmosdb.table.models import Entity
 import sys
+import uuid
 
-# Does a look up based on imageTag for the release pipeline to update its details
+# Does a look up based on filter_name:filter_value for the pipeline to update its details
 def update_pipeline(account_name, account_key, table_name, filter_name, filter_value, name1, value1, name2=None, value2=None):
     table_service = TableService(account_name=account_name, account_key=account_key)
     tasks = table_service.query_entities(table_name, filter=filter_name + " eq '"+ filter_value + "'")
     for task in tasks:
-        print(task.RowKey)
-        print(task.PartitionKey)
-
-        print(task)
+        add = False
+        if task[name1] != None and task[name1] != value1:
+            add = True
         task[name1] = value1
+
         if name2 != None and value2 != None:
+            if task[name2] != None and task[name2] != value2:
+                add = True
             task[name2] = value2
-        table_service.update_entity(table_name, task)
-        print("Updated")
+        
+        if add == False:
+            table_service.update_entity(table_name, task)
+            print("Updating existing entry")
+        else:
+            guid = str(uuid.uuid4()).split('-')[-1]
+            task["RowKey"] = guid
+            table_service.insert_entity(table_name, task)
+            print("Adding new entry since one already existed")
+        print(task)
         break
 
     print("Done")
